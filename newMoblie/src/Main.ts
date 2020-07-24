c218b71d0db898b575b200ff20bf006155251f32
Wayne <wayne@gamesmartltd.com>
2020-07-24 16:41:22 +0800

    turbo90

--- newMoblie/src/Main.ts
+++ newMoblie/src/Main.ts
@@ -39,7 +39,7 @@ class Main extends egret.DisplayObjectContainer {
      * Create a game scene
      */
     private createGameScene() {
-        this.showGame( "SuperLotto", "bin-debug/bingoGames/SuperLotto.js", "resource/default.res.json" );
+        this.showGame( "Turbo90", "bin-debug/bingoGames/Turbo90.js", "resource/default.res.json" );
 		IBingoServer.serverInit();
     }
 
---
+++ newMoblie/src/bingoGames/v2games/Turbo90.ts
@@ -0,0 +1,235 @@
+class Turbo90 extends V2Game{
+
+    protected static get classAssetName(){
+		return "turbo90";
+	}
+
+    protected static get animationAssetName(){
+		return "turbo90Animation";
+	}
+
+	public constructor( assetsPath: string ) {
+		super( "turbo90.conf", assetsPath, 39 );
+        this.ptFilterConfig = "turbo90_filt";
+
+        PaytableUI.textBold = true;
+
+        GameCard.bgRound = 20;
+
+        GameCard.cardTexPosition = new egret.Point( 10, 2 );
+        GameCard.betTexPosition = new egret.Point( 120, 2 );
+        GameCard.texSize = 17;
+        GameCard.texColor = 0xFFFFFF;
+        GameCard.showTitleShadow = new egret.DropShadowFilter(1, 0, 0x000000, 1, 2, 2);
+
+        CardGrid.defaultBgColor = 0xFFFFFF;
+        CardGrid.defaultNumberSize = 28;
+
+        CardGrid.blinkColors1 = 0xFFFF00;
+	    CardGrid.blinkColors2 = 0xFF00FF;
+        GameCard.useRedEffect = true;
+
+        let languageText = GameUIItem.languageText;
+        languageText["bingo"] = { en: "B I N G O", es: "B I N G O", pt: "B I N G O" };
+        languageText["double line"] = { en: "D O U B L E L I N E", es: "D O B L E L Í N E A", pt: "L I N H A D U P L A" };
+        languageText["line"] = { en: "L I N E", es: "L Í N E A", pt: "L I N H A" };
+        languageText["four corners"] = { en: "4 C O R N E R S", es: "4 E S Q U I N A S", pt: "4 E S Q U I N A S" };
+
+        this.needListenToolbarStatus = true;
+        this.tipStatusTextPosition = new egret.Rectangle( 180, 195, 412, 18 );
+        this.tipStatusTextColor = 0xCC0000;
+
+        BallManager.ballOffsetY = 3;
+
+        GameToolBar.toolBarY = 920;
+        BingoBackGroundSetting.defaultScale = false;
+        BingoBackGroundSetting.gameMask = new egret.Rectangle( 0, 0, 2000, 1125 );
+	}
+
+    protected init(){
+        super.init();
+
+        let paytableTexts = new Array<egret.TextField>(4);
+        paytableTexts[0] = this.addGameText( 300, 7, 22, 0x46C8F5, "bingo",false, 200, "", 1);
+        paytableTexts[1] = this.addGameText( 300, 28, 22, 0x46C8F5, "double line",false, 200, "", 1);
+        paytableTexts[2] = this.addGameText( 300, 49, 22, 0x46C8F5, "line",false, 200, "", 1);
+        paytableTexts[3] = this.addGameText(300, 70, 22, 0x46C8F5, "four corners", false, 200, "", 1);
+        paytableTexts.map((t) => {
+            t.fontFamily = "fantasy_impact";
+            t.scaleY = 0.9;
+        });
+
+        let betText = this.addGameText(10, 192, GlobelSettings.language === "en"? 24: 20, 0xFEFE00, "bet", true, 100, "", 1);
+        let creditText = this.addGameText(520, 192, GlobelSettings.language === "en"? 24: 20, 0xFEFE00, "credit", true, 100, "", 1);
+        betText.fontFamily = creditText.fontFamily = "fantasy_impact";
+
+        this.betText = this.addGameTextCenterShadow(70, 194, 19, 0xFFFFFF, "bet", true, 120, true, true);
+        this.creditText = this.addGameTextCenterShadow(600, 194, 19, 0xFEFE00, "credit", true, 180, true, true);
+
+        this.runningBallContainer = new egret.DisplayObjectContainer;
+        Com.addBitmapAt( this.runningBallContainer, this.assetStr( "lotto_gate" ), 0, 0 );
+        this.coverRunningBall = Com.addBitmapAt( this.runningBallContainer, this.assetStr( "lotto_gate_front" ), 0, 0 );
+
+        this.buildSuperEbArea( "mega_" + GlobelSettings.language, 119, 58 );
+
+        this.addLineArrows();
+    }
+
+    private arrowMcs: Array<Array<egret.MovieClip>>;
+
+    private addLineArrows(): void{
+        this.arrowMcs = [];
+        for( let i: number = 0; i < 4; i++ ){
+            this.arrowMcs[i] = [];
+            for( let j: number = 0; j < 3; j++ ){
+                let arrowAnimation: egret.MovieClip = Com.addMovieClipAt( this, this._mcf, "arrowAnimationInT90", this.cardPositions[i]["x"] - 23, this.cardPositions[i]["y"] + 27 * ( j + 1 ) );
+                arrowAnimation.stop();
+                arrowAnimation.visible = false;
+                this.arrowMcs[i][j] = arrowAnimation;
+            }
+        }
+    }
+
+    protected showLastBall( ballIndex: number ): void{
+        super.showLastBall( ballIndex );
+        this.showLastBallAt( ballIndex, 19, -10 );
+
+        this.playSound("t90_ball_mp3");
+	}
+
+    protected showLastBallAt( ballIndex: number, x: number, y: number, scale: number = 1 ): void{
+		if( this.runningBallUI && ( this.runningBallContainer ).contains( this.runningBallUI ) ){
+			( this.runningBallContainer ).removeChild( this.runningBallUI );
+		}
+		this.runningBallUI = this.ballArea.getABigBall( ballIndex, "_small", 60 );
+		Com.addObjectAt( this.runningBallContainer, this.runningBallUI, x, y );
+
+        Com.addObjectAt( this.runningBallContainer, this.coverRunningBall, 0, 0 );
+        Com.addObjectAt( this, this.runningBallContainer, 135, 0 );
+
+        this.getChildByName( this.assetStr( "lotto_balls" ) ).visible = false;
+
+        clearTimeout( this.timeoutId );
+        this.timeoutId = setTimeout( this.removeBigBall.bind( this ), 3000 );
+	}
+
+    private timeoutId: number;
+
+    private removeBigBall(){
+        if( this.runningBallUI && ( this.runningBallContainer ).contains( this.runningBallUI ) ){
+			( this.runningBallContainer ).removeChild( this.runningBallUI );
+		}
+        if( this.contains( this.runningBallContainer ) )this.removeChild( this.runningBallContainer );
+        this.getChildByName( this.assetStr( "lotto_balls" ) ).visible = true;
+    }
+
+    protected clearRunningBallUI(): void{
+        if( this.runningBallContainer && this.contains( this.runningBallContainer ) )this.removeChild( this.runningBallContainer );
+	}
+
+    protected afterCheck( resultList: Array<Object> ): void{
+        super.afterCheck( resultList );
+        this.clearArrow();
+        for( let i: number = 0; i < 4; i++ ){
+            if( resultList[i]["line"] && resultList[i]["line"]["unfitIndexs"] ){
+                for( let line in resultList[i]["line"]["unfitIndexs"] ){
+                    let arrow: egret.MovieClip = this.arrowMcs[i][line];
+                    arrow.visible = true;
+                    arrow.gotoAndPlay(1);
+                }
+            }
+        }
+    }
+
+    private clearArrow(): void{
+        for( let i: number = 0; i < 4; i++ ){
+            for( let j: number = 0; j < 3; j++ ){
+                let arrow: egret.MovieClip = this.arrowMcs[i][j];
+                arrow.visible = false;
+                arrow.stop();
+            }
+        }
+    }
+
+    protected startPlay(): void {
+        super.startPlay();
+        this.clearArrow();
+    }
+
+/******************************************************************************************************************************************************************/    
+    protected showJackpot( jackpot: number, jackpotMinBet: number, betConfig: Array<Object> ){
+        this.addChild( this.jackpotArea = new JackpotLayer( new egret.Point( 540, 16 ), jackpot, jackpotMinBet, betConfig, new egret.Point( -15, -15 ), new egret.Rectangle( 0, 40, 200, 26 ), 26, 0xFFC609, new egret.Rectangle( 0, 0, 200, 26 ), 26, 0xd6c576 ) );
+        this.jackpotArea.textBold = false;
+    }
+
+    protected getPaytablesFit( paytabledName: string, callback: Function = null ): void{
+		let soundName = "";
+        switch (paytabledName) {
+            case "double line": soundName = "t90_double_line_mp3";break;
+            case "four corners": soundName = "t90_4corners_mp3";break;
+            case "bingo": soundName = "t90_bingo_mp3";break;
+            case "line": soundName = "t90_line_mp3"; break;
+            default: break;
+        }
+        if (SoundManager.soundOn && soundName !== "") {
+            this.playSound(soundName, 1, callback);
+        } else {
+            callback();
+        }
+	}
+
+    protected onBetChanged(event: egret.Event): void{
+        super.onBetChanged(event);
+        
+        if (event.data["type"] !== 0) this.playSound("t90_bet_mp3");
+	}
+
+    protected hasExtraBallFit(): void {
+        this.stopSound("t90_ball_mp3");
+        if (this.firstHaveExtraBall) {
+            this.firstHaveExtraBall = false;
+            this.playSound("t90_extra_loop_wav", -1);
+        }
+
+        if( this.isMegaBall ){
+            this.superExtraBg.visible = true;
+            this.gameToolBar.megeExtraOnTop( true );
+
+            if( localStorage.getItem( "turbo90_mega" ) ) return;
+            else{
+                localStorage.setItem( "turbo90_mega", "true" );
+                let ev: egret.Event = new egret.Event( "megaFirst" );
+                ev.data = new egret.Rectangle( 287, 196, 45, 45 );
+                this.dispatchEvent( ev );
+            }
+        }
+    }
+    
+    protected roundOver(): void {
+        super.roundOver();
+        this.stopSound("t90_ball_mp3");
+        this.stopSound("t90_extra_loop_wav");
+    }
+
+	protected getExtraBallFit(): void {
+		this.playSound("t90_extra_ball_mp3");
+	}
+
+	protected collectExtraBall(): void {
+		// override
+	}
+
+	protected changeNumberSound(): void {
+		this.playSound("t90_card_mp3");
+	}
+
+    protected onServerData( data: Object ){
+        super.onServerData( data );
+        if( localStorage.getItem( "turbo90_mega" ) ) return;
+        else{
+            try{
+                // RES.loadGroup( "megaForFirst_" + GlobelSettings.language );
+            }catch(e){}
+        }
+    }
+}
