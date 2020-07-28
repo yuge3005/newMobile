c754066835a3ee5260f740e1bb469c47c826fd18
Wayne <wayne@gamesmartltd.com>
2020-07-28 11:22:42 +0800

    improve game loading

--- newMoblie/src/LoadingUI.ts
+++ newMoblie/src/LoadingUI.ts
@@ -5,6 +5,6 @@ class LoadingUI extends egret.Sprite implements RES.PromiseTaskReporter {
     }
 
     public onProgress(current: number, total: number): void {
-        document.getElementById( "loading_progress_div" ).style.width = Math.floor( 639 * ( 0.18 + current / total * 0.72 + 0 ) ) + "px";
+        document.getElementById( "loading_progress_div" ).style.width = Math.floor( 320 * ( 0.18 + current / total * 0.72 + 0 ) ) + "px";
     }
 }
