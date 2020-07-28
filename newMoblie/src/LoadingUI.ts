312438c5495882d66dc0167003256d427a01d193
Wayne <wayne@gamesmartltd.com>
2020-07-28 11:07:30 +0800

    improve game loader

--- newMoblie/src/LoadingUI.ts
+++ newMoblie/src/LoadingUI.ts
@@ -2,21 +2,9 @@ class LoadingUI extends egret.Sprite implements RES.PromiseTaskReporter {
 
     public constructor() {
         super();
-        this.createView();
-    }
-
-    private textField: egret.TextField;
-
-    private createView(): void {
-        this.textField = new egret.TextField();
-        this.addChild(this.textField);
-        this.textField.y = 300;
-        this.textField.width = 480;
-        this.textField.height = 100;
-        this.textField.textAlign = "center";
     }
 
     public onProgress(current: number, total: number): void {
-        this.textField.text = `Loading...${current}/${total}`;
+        document.getElementById( "loading_progress_div" ).style.width = Math.floor( 639 * ( 0.18 + current / total * 0.72 + 0 ) ) + "px";
     }
 }
