134eb29b38e0bc6059d0c0b9c519e3aefedb33d5
Wayne <wayne@gamesmartltd.com>
2020-07-31 13:56:00 +0800

    mask for grid

--- newMoblie/src/bingoGames/v2games/nineball/NineballCard.ts
+++ newMoblie/src/bingoGames/v2games/nineball/NineballCard.ts
@@ -1,5 +1,23 @@
 class NineballCard extends GameCard{
+
+	private gridLayer: egret.DisplayObjectContainer;
+	private gridMask: egret.Bitmap;
+
 	public constructor( cardId: number ) {
 		super( cardId );
+	}
+
+	public getNumbers( numbers: Array<number> ){
+		super.getNumbers( numbers );
+
+		if( this.gridLayer ) return;
+		this.gridLayer = new egret.DisplayObjectContainer;
+		for( var i: number = 0; i < numbers.length; i++ ){
+			this.gridLayer.addChild( this.grids[i] );
+		}
+		this.addChild( this.gridLayer );
+
+		var gridMask: egret.Bitmap = Com.addBitmapAt( this, BingoMachine.getAssetStr( "mask" ), 13, 55 );
+		this.gridLayer.mask = gridMask;
 	}
 }
