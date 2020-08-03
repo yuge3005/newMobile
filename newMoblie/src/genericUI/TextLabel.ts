1c0f3be2933a51649e6af5eb2566950afc5229e4
Wayne <wayne@gamesmartltd.com>
2020-08-03 09:38:35 +0800

    fix text label

--- newMoblie/src/genericUI/TextLabel.ts
+++ newMoblie/src/genericUI/TextLabel.ts
@@ -11,7 +11,7 @@ class TextLabel extends egret.TextField{
 	public setText( str: string ): void{
 		this.text = str;
 		this.width = this.maxWidth * 5;
-		if( this.size <= this.height - 2 ) this.size = this.height;
+		if( this.size <= this.maxSize - 2 ) this.size = this.maxSize;
 		while( this.textWidth > this.maxWidth ){
 			this.size -= 2;
 		}
