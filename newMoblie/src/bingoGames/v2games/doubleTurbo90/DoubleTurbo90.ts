abf8cc4e945753037e44fa844d49dbf2e292a9d4
Wayne <wayne@gamesmartltd.com>
2020-08-31 17:48:58 +0800

    drop coins

--- newMoblie/src/bingoGames/v2games/doubleTurbo90/DoubleTurbo90.ts
+++ newMoblie/src/bingoGames/v2games/doubleTurbo90/DoubleTurbo90.ts
@@ -90,10 +90,18 @@ class DoubleTurbo90 extends V2Game{
     protected getPaytablesFit( paytabledName: string, callback: Function = null ): void{
 		let soundName = "";
         switch (paytabledName) {
-            case "line": soundName = "t90_line_mp3"; break;
-            case "double line": soundName = "t90_double_line_mp3"; break;
-            case "four corners": soundName = "t90_4corners_mp3"; break;
-            case "bingo": soundName = "t90_bingo_mp3"; break;
+            case "line": soundName = "t90_line_mp3";
+                this.dropCoinsAt( 516, 128, 1 );
+                break;
+            case "double line": soundName = "t90_double_line_mp3";
+                this.dropCoinsAt( 516, 90, 2 );
+                break;
+            case "four corners": soundName = "t90_4corners_mp3";
+                this.dropCoinsAt( 516, 168, 1 );
+                break;
+            case "bingo": soundName = "t90_bingo_mp3";
+                this.dropCoinsAt( 516, 51, 3 );
+                break;
             default: break;
         }
         if (SoundManager.soundOn && soundName !== "") {
