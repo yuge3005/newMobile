11e425a76edf11824d1935dddd320fb1ca4417c2
Wayne <wayne@gamesmartltd.com>
2020-09-18 11:38:24 +0800

    card text

--- newMoblie/src/bingoGames/v2games/pachinko/PachinkoCard.ts
+++ newMoblie/src/bingoGames/v2games/pachinko/PachinkoCard.ts
@@ -4,14 +4,13 @@ class PachinkoCard extends ExtraBlinkCard {
     }
 
     public set bet( value: number ){
-		if (!this.betText) return;
-		this.betText.text = Utils.formatCoinsNumber( value );
+		this.betText.setText( Utils.formatCoinsNumber( value ) );
     }
     
     protected onAdd(event: egret.Event) {
         super.onAdd(event);
 
-        if (this.cardText) this.cardText.text = MuLang.getText( "bet" );
+		if (this.cardText) this.cardText.setText( MuLang.getText( "bet" ) + ":" );
         if (this.betText) this.betText.textColor = 0xFFFF00;
     }
 
