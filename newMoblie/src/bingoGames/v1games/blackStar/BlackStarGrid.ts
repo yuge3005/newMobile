d7ee8635324b510f6124116ef0ec7704353cfcbe
Wayne <wayne@gamesmartltd.com>
2020-09-28 10:16:21 +0800

    grid blink

--- newMoblie/src/bingoGames/v1games/blackStar/BlackStarGrid.ts
+++ newMoblie/src/bingoGames/v1games/blackStar/BlackStarGrid.ts
@@ -6,4 +6,10 @@ class BlackStarGrid extends ExtraBlinkGrid{
 	protected getBlinkBg(): egret.Bitmap{
 		return Com.createBitmapByName( BingoMachine.getAssetStr( TowerGrid.blink2PicName ) );
 	}
+
+	protected buildSmallWinText(){
+		this.extraBlinkNumTxt = Com.addLabelAt( this.extraBinkSp, 0, 3, CardGrid.gridSize.x, 30, 30, false, true );
+		this.smallWinTimesText = Com.addLabelAt( this.extraBinkSp, 0, 30, CardGrid.gridSize.x, 40, 35 );
+		this.smallWinTimesText.textColor = 0xFFFF00;
+	}
 }
