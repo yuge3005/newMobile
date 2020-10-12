3285ad2a3ba0d3c36a2663a7c21ea41d28a9c4ae
Wayne <wayne@gamesmartltd.com>
2020-10-12 13:01:42 +0800

    add unfitEffectLayer

--- newMoblie/src/bingoGames/v1games/blackStar/BlackStarCard.ts
+++ newMoblie/src/bingoGames/v1games/blackStar/BlackStarCard.ts
@@ -1,4 +1,7 @@
 class BlackStarCard extends ExtraBlinkCard{
+
+	private unFitEffectLayer: egret.DisplayObjectContainer;
+
 	public constructor( cardId: number ) {
 		super( cardId );
 	}
@@ -9,11 +12,22 @@ class BlackStarCard extends ExtraBlinkCard{
 		if (this.cardText) this.cardText.setText( MuLang.getText( "card" ) + ":" );
 
 		Com.addBitmapAt( this, BingoMachine.getAssetStr( "card_bg03" ), 13, 34 );
+
+		this.unFitEffectLayer = new egret.DisplayObjectContainer;
+		this.addChildAt( this.unFitEffectLayer, this.getChildIndex( this.fitEffectLayer ) );
 	}
 
 	public getBgColor(){
 		if( GameCard.titleColors ){
 			this.bg.filters = [ MatrixTool.colorMatrixPure( GameCard.titleColors[GameCard.currentBgColorIndex] ) ];
+		}
+	}
+
+	public clearFitEffect(){
+		super.clearFitEffect();
+		if( this.unFitEffectLayer ){
+			this.unFitEffectLayer.removeChildren();
+			this.addChildAt( this.unFitEffectLayer, this.getChildIndex( this.fitEffectLayer ) );
 		}
 	}
 }
