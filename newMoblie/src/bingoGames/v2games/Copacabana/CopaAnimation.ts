a79b06375793875121d7b57bb219c13ebf6cfe34
Wayne <wayne@gamesmartltd.com>
2020-10-29 15:18:29 +0800

    copa animation

--- newMoblie/src/bingoGames/v2games/Copacabana/CopaAnimation.ts
+++ newMoblie/src/bingoGames/v2games/Copacabana/CopaAnimation.ts
@@ -9,16 +9,15 @@ class CopaAnimation extends egret.Sprite{
 		super();
 
 		this.assetName = assetName;
-		this.letterBg = Com.addBitmapAt( this, assetName + ".bg", 0, 240 );
+		this.letterBg = Com.addBitmapAtMiddle( this, assetName + ".bg", 0, 550 );
 		if( fillRepeat )this.letterBg.fillMode = egret.BitmapFillMode.REPEAT;
-		this.letterBg.anchorOffsetY = this.letterBg.height >> 1;
 		this.letterBg.scaleY = 0.01;
-		this.letterBg.width = 755;
+		this.letterBg.width = 2000 + this.letterBg.width;
 		this.letterBg.height = 120;
 
-		this.addTween( this.letterBg, { "scaleY": 1 }, 250, 100, this.addTitle.bind(this), true );
+		this.addTween( this.letterBg, { "scaleY": 1.5 }, 250, 100, this.addTitle.bind(this), true );
 
-		GraphicTool.drawRect( this, new egret.Rectangle( 0, 0, 755, 600 ), 0, false, 0.2 );
+		GraphicTool.drawRect( this, BingoBackGroundSetting.gameMask, 0, false, 0.2 );
 		this.touchEnabled = true;
 
 		this.starParticle = new particle.GravityParticleSystem(RES.getRes("pipaParticle_png"), RES.getRes("pipaParticle_json"));
@@ -36,20 +35,16 @@ class CopaAnimation extends egret.Sprite{
 	}
 
 	private addTitle(){
-		let tittle: egret.Bitmap = Com.addBitmapAt( this, this.assetName + ".title_" + GlobelSettings.language, 800, 220 );
-
-		this.addTween( tittle, { "x": 775 - tittle.width >> 1 }, 300, 100, this.titleLeave.bind(this, tittle), true );
+		let tittle: egret.Bitmap = Com.addBitmapAtMiddle( this, this.assetName + ".title_" + GlobelSettings.language, 2500, 550 );
+		tittle.scaleX = tittle.scaleY = 2;
+		this.addTween( tittle, { "x": 1000 }, 300, 100, this.titleLeave.bind(this, tittle), true );
 	}
 
 	private titleLeave( tittle: egret.Bitmap ){
-		this.addTween( tittle, { "x": - tittle.width - 50 }, 300, 1500, this.bgLeave.bind(this), false );
+		this.addTween( tittle, { "x": - 500 }, 300, 1500, this.bgLeave.bind(this), false );
 	}
 
 	private bgLeave(): void{
-		this.addTween( this.letterBg, { "scaleY": 0.01 }, 250, 100, this.deleteSelf.bind(this), false );
-	}
-
-	private deleteSelf(): void{
-		if( this.parent )this.parent.removeChild( this );
+		this.addTween( this.letterBg, { "scaleY": 0.01 }, 250, 100, MDS.removeSelf.bind( this, this ), false );
 	}
 }
