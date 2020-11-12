6bfe101983215a27db881a5c28dd56daa54dceef
Wayne <wayne@gamesmartltd.com>
2020-11-12 15:03:43 +0800

    sound and filter config

--- newMoblie/src/bingoGames/v2games/aztecAndPharos/Aztec.ts
+++ newMoblie/src/bingoGames/v2games/aztecAndPharos/Aztec.ts
@@ -11,7 +11,7 @@ class Aztec extends AztecPharosSuper{
 	public constructor( assetsPath: string ) {
 		super( "aztec.conf", assetsPath, 50 );
 
-        CardGridColorAndSizeSettings.defaultNumberSize = 50;
+        CardGridColorAndSizeSettings.defaultNumberSize = 45;
 
         BallManager.ballOffsetY = 8;
         BallManager.textBold = true;
@@ -44,22 +44,6 @@ class Aztec extends AztecPharosSuper{
 
     protected showJackpot( jackpot: number, jackpotMinBet: number, betConfig: Array<Object> ){
         this.addChild( this.jackpotArea = new JackpotLayer( new egret.Point( 20, 205 ), jackpot, jackpotMinBet, betConfig, new egret.Point( 2, 18 ), new egret.Rectangle( 0, 45, 165, 20 ), 20, 0xd6c576, new egret.Rectangle( 0, 0, 165, 20 ), 20, 0xeddb93 ) );
-    }
-
-    protected getPaytablesFit(paytabledName: string, callback: Function = null): void{
-        let soundName = "";
-        switch (paytabledName) {
-            case "one side": soundName = "azt10_wav"; break;
-            case "two side": soundName = "azt9_wav"; break;
-            case "three side": soundName = "azt2_wav"; break;
-            case "bingo": soundName = "azt6_wav";break;
-            default: break;
-        }
-        if (SoundManager.soundOn && soundName !== "") {
-            this.playSound(soundName, 1, callback);
-        } else {
-            callback();
-        }
     }
     
     protected hasExtraBallFit(): void {
