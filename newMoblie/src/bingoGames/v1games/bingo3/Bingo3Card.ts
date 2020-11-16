54689415a779466451962541d72d277e1e322df3
Wayne <wayne@gamesmartltd.com>
2020-11-16 13:26:40 +0800

    card text settings

--- newMoblie/src/bingoGames/v1games/bingo3/Bingo3Card.ts
+++ newMoblie/src/bingoGames/v1games/bingo3/Bingo3Card.ts
@@ -3,6 +3,15 @@ class Bingo3Card extends GameCard{
 		super( cardId );
 	}
 
+	protected onAdd(event: egret.Event) {
+        super.onAdd(event);
+
+		this.cardText.fontFamily = "Arial";
+		this.cardText.bold = true;
+		this.betText.fontFamily = "Arial";
+		this.betText.bold = true;
+	}
+
 	public getBgColor(){
 		if( GameCardUISettings.titleColors ){
 			this.bg.filters = [ MatrixTool.colorMatrixPure( GameCardUISettings.cardTitleColor ) ];
