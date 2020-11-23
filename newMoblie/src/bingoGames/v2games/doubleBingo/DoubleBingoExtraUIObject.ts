7d4ebdde40c8af228ccf2095c81c9e08dd4bb757
Wayne <wayne@gamesmartltd.com>
2020-11-23 14:54:23 +0800

    extra animation

--- newMoblie/src/bingoGames/v2games/doubleBingo/DoubleBingoExtraUIObject.ts
+++ newMoblie/src/bingoGames/v2games/doubleBingo/DoubleBingoExtraUIObject.ts
@@ -14,12 +14,28 @@ class DoubleBingoExtraUIObject extends egret.DisplayObjectContainer{
 		let uiIndex: number = oldUI.parent.getChildIndex( this.bg );
 		oldUI.parent.addChildAt( this, uiIndex );
 		this.addChild( this.bg );
+
+		this.redBallBgs = [];
+		for( let i: number = 0; i < 12; i++ ){
+			let pt: egret.Point = BallManager.getBallLastPosition( i + 31 );
+			this.redBallBgs[i] = Com.addBitmapAt( this, BingoMachine.getAssetStr("red_ballpng"), pt.x + 2, pt.y + 2 );
+		}
 	}
 
 	public showExtraUI( show: boolean = true ){
-		if( show ){
-			TweenerTool.tweenTo( this, { alpha: 1 }, 300 );
+		TweenerTool.tweenTo( this, { alpha: show ? 1 : 0 }, 300 );
+	}
+
+	public showRedBallsAnimation(){
+		let interval: number = 100;
+		for( let i: number = 0; i < 12; i++ ){
+			this.redBallBgs[i].visible = true;
+			this.redBallBgs[i].alpha = 0;
+			let tw: egret.Tween = egret.Tween.get( this.redBallBgs[i] );
+			tw.wait( i * interval );
+			tw.to( { alpha: 1 }, 33 );
+			tw.wait( ( 12 - i ) * interval * 2 );
+			tw.to( { alpha: 0 }, 33 );
 		}
-        else TweenerTool.tweenTo( this, { alpha: 0 }, 300 );
 	}
 }
