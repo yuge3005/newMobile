46272d608a83690004500f9a26de96d54534979d
Wayne <wayne@gamesmartltd.com>
2020-12-03 14:52:22 +0800

    bingo button position

--- MultiBingo/src/multiPlayerGames/multigames/multiBingo/MultiPlayerBingoCard.ts
+++ MultiBingo/src/multiPlayerGames/multigames/multiBingo/MultiPlayerBingoCard.ts
@@ -53,7 +53,7 @@ class MultiPlayerBingoCard extends Multi75Card{
 		this.bingoMask.touchEnabled = true;
 		this.addChild( this.bingoMask );
 		Com.addBitmapAt( this.bingoMask, MultiPlayerMachine.getAssetStr( "BINGO-BG" ), 0, 0 );
-		Com.addDownButtonAt( this.bingoMask, MultiPlayerMachine.getAssetStr( "BINGO" ), MultiPlayerMachine.getAssetStr( "BINGO" ), this.bg.width >> 1, this.bg.height >> 1, this.callBingo.bind(this), true );
+		Com.addDownButtonAt( this.bingoMask, MultiPlayerMachine.getAssetStr( "BINGO" ), MultiPlayerMachine.getAssetStr( "BINGO" ), this.bg.width - 508 >> 1, this.bg.height >> 1, this.callBingo.bind(this), true );
 	}
 
 	private callBingo(){
