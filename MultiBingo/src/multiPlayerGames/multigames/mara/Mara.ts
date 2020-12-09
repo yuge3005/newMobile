3d9844c27b8418c2496b085dcf4889d4f3f71caf
Wayne <wayne@gamesmartltd.com>
2020-12-09 16:11:50 +0800

    card settings

--- MultiBingo/src/multiPlayerGames/multigames/mara/Mara.ts
+++ MultiBingo/src/multiPlayerGames/multigames/mara/Mara.ts
@@ -32,7 +32,7 @@ class Mara extends Multi75Super{
 		MultiPlayerGrid.linePicName = "orange";
 		MultiPlayerGrid.zeroUIName = "zero_grid";
 
-		MultiPlayerGrid.defaultNumberSize = 44;
+		MultiPlayerGrid.defaultNumberSize = 40;
 
 		this.cardClickMode( false );
 	}
