dc74dfd7783bbe05052c46e30d8e64bfcb9b658d
Wayne <wayne@gamesmartltd.com>
2020-12-10 17:47:59 +0800

    change font

--- MultiBingo/src/multiPlayerGames/multigames/mara/MaraBingoInfoBar.ts
+++ MultiBingo/src/multiPlayerGames/multigames/mara/MaraBingoInfoBar.ts
@@ -17,8 +17,9 @@ class MaraBingoInfoBar extends MultyBingoInfoBar{
 		Com.addBitmapAt( this.bgTip, "mara_chat_box_json.number of people", 102, 0 );
 		let txCity: TextLabel = MDS.addGameText( this.bgTip, 132, 90, 60, 0xffb500, "City of Light", false, 400 );
 		txCity.textAlign = "center";
+		txCity.fontFamily = "Righteous";
 		txCity.filters = [ new egret.DropShadowFilter( 3, 135, 0, 0.5, 4, 4, 2, 1 ) ];
-		txCity.text = txCity.text.toUpperCase();
+		txCity.setText( txCity.text.toUpperCase() );
 		this.addChildAt( this.bgLight, 0 );
 		this.addChildAt( Com.addBitmapAt( this, "mara_chat_box_json.photo frame", 0, 0 ), 0 );
 		this.addChildAt( this.bgTip, 0 );
