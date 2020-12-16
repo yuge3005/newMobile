8bda6efc571874073df46325d6a51098657b5ae1
Wayne <wayne@gamesmartltd.com>
2020-12-16 10:46:22 +0800

    delete code

--- MultiBingo/src/Trigger.ts
+++
@@ -1,5 +0,0 @@
-class Trigger {
-	
-	public static flyingCoins(coinsCount: number, startPosition: egret.Point, middlePosition: egret.Point, startScale: number, endScale: number, middleScale: number, callback: Function = null) {
-	}
-}
