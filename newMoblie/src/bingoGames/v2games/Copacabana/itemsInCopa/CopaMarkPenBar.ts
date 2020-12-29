332b3f847d32912c9dff685cfe4411d1c9fd3707
Wayne <wayne@gamesmartltd.com>
2020-12-29 13:20:36 +0800

    pen

--- newMoblie/src/bingoGames/v2games/Copacabana/itemsInCopa/CopaMarkPenBar.ts
+++ newMoblie/src/bingoGames/v2games/Copacabana/itemsInCopa/CopaMarkPenBar.ts
@@ -1,25 +1,25 @@
 class CopaMarkPenBar extends egret.DisplayObjectContainer{
 
-	private markPen1: egret.Bitmap;
-    private markPen2: egret.Bitmap;
+    private markPen1: egret.DisplayObjectContainer;
+    private markPen2: egret.DisplayObjectContainer;
 
-	private bigPen1: egret.Bitmap;
+    private bigPen1: egret.Bitmap;
     private bigPen2: egret.Bitmap;
 
-	private bigMarkPenContainer1: egret.DisplayObjectContainer;
+    private bigMarkPenContainer1: egret.DisplayObjectContainer;
     private bigMarkPenContainer2: egret.DisplayObjectContainer;
 
-	public needMarkLine: boolean;
+    public needMarkLine: boolean;
     public markColumn: Array<number>;
 
     private markNumber1: number;
     private markNumber2: number;
 
-	public constructor() {
-		super();
+    public constructor() {
+        super();
 
-		this.markPen1 = Com.addBitmapAt( this, BingoMachine.getAssetStr( "icon_mark_big" ), 0, 0 );
-        this.markPen2 = Com.addBitmapAt( this, BingoMachine.getAssetStr( "icon_mark_big" ), 0, 0 );
+        this.markPen1 = this.buildAPen();
+        this.markPen2 = this.buildAPen();
         this.markPen1.visible = this.markPen2.visible = false;
 
         this.bigMarkPenContainer1 = new egret.DisplayObjectContainer;
@@ -27,36 +27,47 @@ class CopaMarkPenBar extends egret.DisplayObjectContainer{
         Com.addObjectAt( this, this.bigMarkPenContainer1, 0, 0 );
         Com.addObjectAt( this, this.bigMarkPenContainer2, 0, 0 );
 
-		this.addEventListener( egret.Event.ADDED_TO_STAGE, this.onAdd, this );
-	}
+        this.addEventListener( egret.Event.ADDED_TO_STAGE, this.onAdd, this );
+    }
 
-	private onAdd( event: egret.Event ){
-		this.addEventListener( egret.Event.ENTER_FRAME, this.markColumnEveryFrame, this );
-		this.removeEventListener( egret.Event.ADDED_TO_STAGE, this.onAdd, this );
-	}
+    private buildAPen(): egret.DisplayObjectContainer{
+        let pen: egret.DisplayObjectContainer = new egret.DisplayObjectContainer;
+        let sq: egret.Sprite = new egret.Sprite;
+        GraphicTool.drawRect( sq, new egret.Rectangle( 0, 0, CardGridColorAndSizeSettings.gridSize.x, CardGridColorAndSizeSettings.gridSize.y ), 0xCCCC00, false, 0.25 );
+        pen.addChild( sq );
+        let penBitmap: egret.Bitmap = Com.addBitmapAt( pen, BingoMachine.getAssetStr( "icon_mark_big" ), 70, -40 );
+        penBitmap.scaleX = penBitmap.scaleY = 1.43;
+        Com.addObjectAt( this, pen, 0 , 0 );
+        return pen;
+    }
 
-	public start(){
-		this.markPen1.visible = true;
-		this.markPen2.visible = true;
-		this.bigMarkPenContainer1.removeChildren();
-		this.bigMarkPenContainer2.removeChildren();
-	}
+    private onAdd( event: egret.Event ){
+        this.addEventListener( egret.Event.ENTER_FRAME, this.markColumnEveryFrame, this );
+        this.removeEventListener( egret.Event.ADDED_TO_STAGE, this.onAdd, this );
+    }
 
-	private markColumnEveryFrame( event: egret.Event ): void{
+    public start(){
+        this.markPen1.visible = true;
+        this.markPen2.visible = true;
+        this.bigMarkPenContainer1.removeChildren();
+        this.bigMarkPenContainer2.removeChildren();
+    }
+
+    private markColumnEveryFrame( event: egret.Event ): void{
         if( this.bigPen1 && this.bigPen1.parent ) this.addPenPoint( this.bigPen1 );
         if( this.bigPen2 && this.bigPen2.parent ) this.addPenPoint( this.bigPen2 );
 
         if( !this.stage ) this.removeEventListener( egret.Event.ENTER_FRAME, this.markColumnEveryFrame, this );
     }
 
-	private addPenPoint( pen: egret.Bitmap ): void{
+    private addPenPoint( pen: egret.Bitmap ): void{
         let pt: egret.Bitmap = Com.createBitmapByName( BingoMachine.getAssetStr( "marker_lightball" ) );
         pt.x = pen.x + Math.random() * 6 - 7;
         pt.y = pen.y + 31;
         pen.parent.addChildAt( pt, 0 );
     }
 
-	public showMarkPen( isMark: boolean ){
+    public showMarkPen( isMark: boolean ){
         if( isMark ){
             let indexPt1: egret.Point = GameCardUISettings.getIndexOnCard( this.markColumn[0] );
             this.markNumber1 = GameCardUISettings.numberAtCard( indexPt1.x, indexPt1.y );
@@ -72,7 +83,7 @@ class CopaMarkPenBar extends egret.DisplayObjectContainer{
             this.bigMarkPenContainer2.removeChildren();
         }
         this.markPen1.visible = this.markPen2.visible = this.needMarkLine = isMark;
-	}
+    }
 
     public checkColumNumbers( ballIndex: number ): egret.Point{
         if( ballIndex == this.markNumber1 || ballIndex == this.markNumber2 ){
@@ -83,7 +94,7 @@ class CopaMarkPenBar extends egret.DisplayObjectContainer{
         return null;
     }
 
-	public getColumnNumbers( card1: number, grid1: number ): void{
+    public getColumnNumbers( card1: number, grid1: number ): void{
         let pt: egret.Point = GameCardUISettings.positionOnCard( card1, grid1 );
         let paper: egret.DisplayObjectContainer = card1 ? this.bigMarkPenContainer1 : this.bigMarkPenContainer2;
         let pen: egret.Bitmap = Com.addBitmapAt( paper, BingoMachine.getAssetStr( "icon_mark_big" ), pt.x + 55, pt.y - 12 );
