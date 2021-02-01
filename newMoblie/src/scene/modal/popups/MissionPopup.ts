035bdc3e0ca7676a5dbf585fa6ab32e21ec29f72
Wayne <wayne@gamesmartltd.com>
2021-02-01 15:23:33 +0800

    mission popup

--- newMoblie/src/scene/modal/popups/MissionPopup.ts
+++ newMoblie/src/scene/modal/popups/MissionPopup.ts
@@ -1,5 +1,7 @@
 class MissionPopup extends GenericPo {
 
+	private taskListLayer: egret.DisplayObjectContainer;
+
 	protected static get classAssetName() {
 		return "bingoMissionPopup";
 	}
@@ -10,7 +12,30 @@ class MissionPopup extends GenericPo {
 
 	protected init() {
         this.bgAssetName = "missionBingo_json.bg";
+		this.closeButtonAssetName = "missionPopup_json.btn_close";
+
+		this.closeButtonOffset = new egret.Point( 190, 18 );
 
         super.init();
+
+		this.anchorOffsetX = 922;
+		this.anchorOffsetY = 472;
+
+		Com.addBitmapAtMiddle( this, "missionBingo_json.mission_" + MuLang.language, 797, 41 );
+		Com.addBitmapAt( this, "missionBingo_json.card", 1080, -68 );
+
+		this.taskListLayer = new egret.DisplayObjectContainer;
+		this.addChild( this.taskListLayer );
+		this.showListByData();
+
+		Com.addBitmapAt( this, "missionPopup_json.doctor", 1475, 29 );
+		this.addChild( this.closeButton );
+	}
+
+	private showListByData(){
+		this.taskListLayer.removeChildren();
+
+		let mission: Object = PlayerConfig.player( "mission" );
+		
 	}
 }
