c9fe9c4762b293f14c6335830bdfa367678e6b8a
Wayne <wayne@gamesmartltd.com>
2021-03-09 17:03:57 +0800

    rate bar

--- newMoblie/src/libs/gameSettingPopup/RateBar.ts
+++ newMoblie/src/libs/gameSettingPopup/RateBar.ts
@@ -14,9 +14,14 @@ class RateBar extends egret.Sprite{
 
         this.bg = Com.addBitmapAtMiddle( this, this.langResource + ".M", 0, 0 );
 
+		let title: TextLabel = MDS.addGameText( this, -370, -260, 48, 0x222222, "rate_us_now", false, 740, "", 1 );
+		title.textAlign = "center";
+
+		let tip: TextLabel = MDS.addGameText( this, -370, -180, 35, 0x555555, "how_to_rate", false, 740, "", 1 );
+		tip.textAlign = "center";
+
 		this.closeBtn = Com.addDownButtonAt( this, this.langResource + ".roomCloseButton", this.langResource + ".roomCloseButton", this.bg.width >> 1, -this.bg.height >> 1, this.closeThisBar.bind(this), true );
-        this.closeBtn.x -= this.closeBtn.width >> 1;
-        this.closeBtn.y -= this.closeBtn.height >> 1;
+        this.closeBtn.x -= this.closeBtn.width;
 	}
 
 
