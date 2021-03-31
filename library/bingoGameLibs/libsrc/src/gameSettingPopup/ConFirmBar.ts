c1390bd836cb69f6c080b5ca070a11bdbf63ffd7
Wayne <wayne@gamesmartltd.com>
2021-03-31 17:15:28 +0800

    comfirm bar

--- library/bingoGameLibs/libsrc/src/gameSettingPopup/ConFirmBar.ts
+++ library/bingoGameLibs/libsrc/src/gameSettingPopup/ConFirmBar.ts
@@ -10,16 +10,18 @@ class ConFirmBar extends egret.Sprite{
         barBg.width = 1110;
         barBg.height = 535;
 
-		let title: TextLabel = Com.addLabelAt( this, 100-610, 75-320, 910, 76, 76 );
+		let title: TextLabel = Com.addLabelAt( this, 100-610, 75-320, 910, 72, 72, false, true );
         title.text = MuLang.getText( "change_language" );
         let tip: TextLabel = Com.addLabelAt( this, 100-610, 200-320, 910, 100, 48 );
         tip.setText( MuLang.getText( "change_language_tip" ) );
         let btn1: TouchDownButton = Com.addDownButtonAt( this, "gameSettings_json.OK", "gameSettings_json.OK", 125-610, 370-320, this.closeThisBar.bind(this), true );
-        let btTx1: egret.TextField = Com.addTextAt( this, 0, 0, 20, 72, 72 );
+        let btTx1: egret.TextField = Com.addTextAt( this, 0, 0, 20, 55, 55 );
+        btTx1.fontFamily = "Righteous";
         btTx1.text = MuLang.getText( "cancel" );
         btn1.setButtonText( btTx1 );
 		let btn2: TouchDownButton = Com.addDownButtonAt( this, "gameSettings_json.OK", "gameSettings_json.OK", 5, 370-320, this.confirmChange.bind(this), true );
-        let btTx2: egret.TextField = Com.addTextAt( this, 0, 0, 20, 72, 72 );
+        let btTx2: egret.TextField = Com.addTextAt( this, 0, 0, 20, 55, 55 );
+        btTx2.fontFamily = "Righteous";
 		btTx2.text = MuLang.getText( "confirm" );
         btn2.setButtonText( btTx2 );
 	}
