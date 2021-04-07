68f5aec5f99b12120742f7d4d2e5d8ae17cb2e89
Wayne <wayne@gamesmartltd.com>
2021-04-07 17:38:04 +0800

    halloween assets

--- slots/src/lines/LineManager.ts
+++ slots/src/lines/LineManager.ts
@@ -3,4 +3,8 @@ class LineManager {
 	}
 
 	public static enabledCards: number;
+
+	public static lineStartPoint: egret.Point;
+	public static lineScale: egret.Point = new egret.Point( 3, 3 );
+	public static linePicPositions: Array<number>;
 }
