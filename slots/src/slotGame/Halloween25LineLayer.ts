903b5741a9664e7b38bfe92fe867e8dfb8b5c9f8
Wayne <wayne@gamesmartltd.com>
2021-04-14 15:56:24 +0800

    scale lines

--- slots/src/slotGame/Halloween25LineLayer.ts
+++ slots/src/slotGame/Halloween25LineLayer.ts
@@ -8,15 +8,44 @@ class Halloween25LineLayer extends HalloweenLineLayer{
 		this.paytableFgs = [];
 		for( let i: number = 0; i < 25; i++ ){
 			this.paytableFgs[i] = this.parent.getChildByName( SlotMachine.getAssetStr( "G" + ( i + 1 ) ) );
+			this.paytableFgs[i].scaleX = this.paytableFgs[i].scaleY = 0.9;
+
+			let tempX: number = this.paytableFgs[i].x - 333;
+			let tempY: number = this.paytableFgs[i].y - 124;
+			tempX *= 0.9;
+			tempY *= 0.9;
+			tempX += 403;
+			tempY += 144;
+
+			this.paytableFgs[i].x = Math.floor( tempX );
+			this.paytableFgs[i].y = Math.floor( tempY );
         }
     }
 
-	protected buildLineUI( lineNumber: number ): egret.Bitmap{
-		if( lineNumber <= 20 )return super.buildLineUI( lineNumber );
-		else{
-			let pic: egret.Bitmap = Com.addBitmapAt( this, "halloween_line_json.line" + lineNumber, LineManager.linePicPositions[ lineNumber - 1 ], 0 );
-			pic.height = 760;
-			return pic;
+	public addPaytableUI(){
+		LineManager.getPayTableUI();
+		let pts: Object = LineManager.linesDictionary;
+		for( let ob in pts ){
+			let pos: Object = pts[ob].position;
+			pts[ob].x = ( pos["x"] - 330 ) * 0.9 - 1;
+			pts[ob].y = ( pos["y"] - 124 ) * 0.9 + 7;
+			pts[ob].tx.size = 42;
+			this.addChild( pts[ob] );
 		}
+
+		this.buildFgs();
+	}
+
+	protected buildLineUI( lineNumber: number ): egret.Bitmap{
+		let pic: egret.Bitmap;
+		if( lineNumber <= 20 ){
+			pic = Com.addBitmapAt( this, "halloween_line_json.line" + lineNumber, 0, LineManager.linePicPositions[ lineNumber - 1 ] - 4 - 15 * LineManager.linePicPositions[ lineNumber - 1 ] / 720 );
+			pic.scaleX = pic.scaleY = 2.7;
+		}
+		else{
+			pic = Com.addBitmapAt( this, "halloween_line_json.line" + lineNumber, LineManager.linePicPositions[ lineNumber - 1 ], 0 );
+			pic.height = 760;
+		}
+		return pic;
 	}
 }
